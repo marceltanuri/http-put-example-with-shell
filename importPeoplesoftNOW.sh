bde6aef7df10d3bdb3854e479a96c74b24095f5f
Marcel Tanuri <marceltanuri@C02TX1DYG8WL.local>
2020-09-08 20:02:46 -0300

    add sh file

---
+++ importPeoplesoftNOW.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -u parameterU -e parameterE"
+   echo -e "\t-u Liferay credentials to access the service."
+   echo -e "\t-e Target environment, hk for homologation, prd for production. "
+   exit 1 # Exit script after printing help
+}
+
+while getopts "u:e:" opt
+do
+   case "$opt" in
+      u ) parameterU="$OPTARG" ;;
+      e ) parameterE="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+
+# Print helpFunction in case parameters are empty
+if [ -z "$parameterU" ] || [ -z "$parameterE" ]
+then
+   echo "Some or all of the parameters are empty";
+   helpFunction
+fi
+
+# Begin script
+echo "Inciando importação peoplesoft no NOW..."
+echo "Executando em $env"
+java -jar execute-peoplesoft.jar -e$parameterE -u$parameterU
+echo "\nImportação finalizada."
